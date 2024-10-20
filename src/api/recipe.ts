207f09b97882bd9dec19c1eb301d9478619b1df5
Minyoung <alsendrha@naver.com>
2024-10-20 17:22:41 +0900

    api 연결중

--- src/api/recipe.ts
+++ src/api/recipe.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {baseUrl} from './axios';
+import {AddIngredientType} from '../type/types';
 
 export const getRecipeList = async () => {
   try {
@@ -8,5 +10,24 @@ export const getRecipeList = async () => {
     }
   } catch (error) {
     console.log('추천 레시피 가져오기', error);
+  }
+};
+
+export const addMyRecipe = async (data: AddIngredientType) => {
+  try {
+    const response = await baseUrl.post(
+      'api/board',
+      {data},
+      {
+        headers: {
+          Authorization: `Bearer ${await AsyncStorage.getItem('token')}`,
+        },
+      },
+    );
+    if (response.status === 200) {
+      console.log('레시피 추가 성공');
+    }
+  } catch (error) {
+    console.log('레시피 추가 실패', error);
   }
 };
