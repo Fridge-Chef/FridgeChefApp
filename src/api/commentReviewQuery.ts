bad7d2544704d34ffb8e44e5b62dab223dcdf879
Minyoung <alsendrha@naver.com>
2024-11-17 16:05:21 +0900

    리스트 캐싱문제 수정중

--- src/api/commentReviewQuery.ts
+++ src/api/commentReviewQuery.ts
@@ -39,7 +39,7 @@ export const useGetRecipeReviewDetail = (
 ) => {
   const queryFn = () => recipeReviewDetail(boardId, commentId);
   return useQuery<RecipeReviewDetailType>({
-    queryKey: ['recipeReviewDetail'],
+    queryKey: ['recipeReviewDetail', `${boardId}-${commentId}`],
     queryFn,
   });
 };
